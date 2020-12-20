21be3151fcb19f92e5431fc64d76e4ae89a8b43d
Yuehero <yueyue.guo@outlook.com>
2020-12-21 01:08:57 +0800

    update dp

--- solution/dp.cpp
+++ solution/dp.cpp
@@ -7,18 +7,12 @@
 namespace  leetcode {
 
     /*假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
-    每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢*/
-
+      每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢*/
     int Dp::climbStairs(int n) {
         //dp 自底向上
-        if (n == 0) {
-            return 0;
-        }
         _dp = std::vector<int>(n + 1, -1);
         _dp[0] = 1;
         _dp[1] = 1;
-        _dp[2] = 2;
-
         //求解dp[i]
         for (int i = 2; i <= n; i++) {
             if (_dp[i] == -1) {
